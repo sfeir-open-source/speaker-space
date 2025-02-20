e434eb504fa90bbc484c3777450f99209ef462e9
MarineGiroux <85103261+MarineGiroux@users.noreply.github.com>
2025-02-20 15:02:48 +0100

    rename logout home page, modify navbar with user profile

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import {TestConnectionBackFrontComponent} from './common/test-connection-back-front/test-connection-back-front.component';
-import {HomePageComponent} from './features/home-page/home-page.component';
 import {NotFoundPageComponent} from './features/not-found-page/not-found-page.component';
 import {LoginComponent} from './features/login/login.component';
+import {LogoutHomePageComponent} from './features/logout-home-page/logout-home-page-component';
 
 export const routes: Routes = [
-  { path:'', component: HomePageComponent},
+  { path:'', component: LogoutHomePageComponent},
   { path: 'testco', component: TestConnectionBackFrontComponent },
   { path: 'login', component: LoginComponent },
   { path: 'not-found', component: NotFoundPageComponent },
--- front/src/app/common/components/navbar/navbar.component.ts
+++ front/src/app/common/components/navbar/navbar.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isLogin = false;
+
+  haveNotification = false;
 
 }
