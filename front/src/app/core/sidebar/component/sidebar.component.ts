ce81cc871a6d474449ab4a9087b8fc9ed9039e57
MarineGiroux <85103261+MarineGiroux@users.noreply.github.com>
2025-03-18 15:54:06 +0100

    refactoring of html and css to remove div

--- front/src/app/core/sidebar/component/sidebar.component.ts
+++ front/src/app/core/sidebar/component/sidebar.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { SidebarService } from '../service/sidebar.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../login/services/auth.service';
-import { Router } from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs';
+import {SidebarNavItemComponent} from '../../../shared/sidebar-nav-item/sidebar-nav-item.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarNavItemComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  currentRoute: string = '';
+  hasUnreadNotifications: boolean = true;
+  notificationCount: number = 1;
+
   constructor(
     public sidebarService: SidebarService,
     private authService: AuthService,
     private router: Router
   ) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = event.url;
+    });
+
+    this.currentRoute = this.router.url;
+  }
+
 
   closeSidebar() {
     this.sidebarService.toggleSidebar(false);
@@ -29,6 +46,11 @@ export class SidebarComponent {
 
   navigateTo(path: string) {
     this.router.navigate([path]);
+    this.closeSidebar();
+  }
+
+  createNewTeam() {
+    console.log('Creating new team...');
     this.closeSidebar();
   }
 }
